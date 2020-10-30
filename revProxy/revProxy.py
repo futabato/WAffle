e9b364ee3e4ea8a9157e1308d995303026188aab
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-10-30 14:13:30 +0900

    リバースプロキシWAFのサンプル

---
+++ revProxy/revProxy.py
@@ -0,0 +1,24 @@
+import requests
+from flask import *
+from requests.api import request
+from werkzeug.routing import BaseConverter
+
+app = Flask(__name__)
+
+class RegexConverter(BaseConverter):
+  def __init__(self, url_map, *items):
+    super(RegexConverter, self).__init__(url_map)
+    self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
+
+@app.route('/<regex(".*"):path>')
+def proxy(path):
+    if "<" in path:
+        print(path)
+        return render_template('waffle.html')
+    url = "http://localhost:80/"+path
+    r = requests.get(url)
+    return Response(r.content)
+
+app.run()
