988ebc6218538de9d2c9bcd5e36b79f4e15ddeb3
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-10-30 17:51:34 +0900

    blacklistを正規表現にした

--- revProxy/revProxy.py
+++ revProxy/revProxy.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from flask import Flask, Response, render_template
 from requests.api import request
@@ -6,9 +8,9 @@ from werkzeug.routing import BaseConverter
 app = Flask(__name__)
 
 class RegexConverter(BaseConverter):
-  def __init__(self, url_map, *items):
-    super(RegexConverter, self).__init__(url_map)
-    self.regex = items[0]
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
 
 app.url_map.converters['regex'] = RegexConverter
 
@@ -17,10 +19,12 @@ with open("blacklist.txt") as f:
 
 @app.route('/<regex(".*"):path>')
 def proxy(path):
-    print(path in bList)
-    if path in bList:
-        print(path)
-        return render_template('waffle.html')
+
+    for val in bList:
+        m = re.match(val, path, re.IGNORECASE)
+        if m != None :
+            return render_template('waffle.html')    
+
     url = "http://localhost:80/"+path
     r = requests.get(url)
     return Response(r.content)
