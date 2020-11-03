c0f239178ba8976317636b5dcb2f08f45ee42d4b
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-11-03 23:15:18 +0900

    WAFテスト用の脆弱性なphpたち

---
+++ vuln/receive.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>post</title>
+</head>
+
+<body>
+	<?php
+	$input = $_POST["input"];
+	$input2 = $_POST["input2"];
+	print $input.",".$input2;
+	?>
+</body>
+
+</html>
