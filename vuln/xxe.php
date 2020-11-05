23075b6fe3ceedb280797d1ce9c94ec2eb439333
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-11-05 15:38:02 +0900

    xxeなんか上手くいかんけど、眠いのでpushだけしちゃう

---
+++ vuln/xxe.php
@@ -0,0 +1,11 @@
+<?php
+$doc = new DOMDocument();
+$doc->load($_FILES['user']['tmp_name']);
+$name = $doc->getElementsByTagName('name')->item(0)->textContent;
+$addr = $doc->getElementsByTagName('address')->item(0)->textContent;
+?>
+
+<body>
+    name: <?php echo htmlspecialchars($name); ?><br>
+    bio: <?php echo htmlspecialchars($addr); ?><br>
+</body>
