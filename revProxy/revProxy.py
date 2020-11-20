5089a1e844817ae687e31cd26aaa3f7cb3e1cdb9
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-11-20 14:19:25 +0900

    使ってないパッケージを削除

--- revProxy/revProxy.py
+++ revProxy/revProxy.py
@@ -2,10 +2,7 @@ import datetime
 import re
 import subprocess
 
-import requests
-from flask import Flask, Response, render_template
-from flask import request as req
-from requests.api import request
+from flask import Flask, Response, render_template, request
 from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
@@ -31,17 +28,17 @@ url = "http://localhost:8080/"
 
 @app.route('/<regex(".*"):path>', methods=["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"])
 def post(path):
-    query = req.query_string
+    query = request.query_string
     if query != b'' :
         path += "?" + query.decode()
 
-    if waf(path, req.get_data().decode()):
+    if waf(path, request.get_data().decode()):
         return render_template('waffle.html')
 
     try :
-        proc = subprocess.run(["curl", "-X", req.method, url+path,"-H","Content-Type:" + req.headers.getlist("Content-Type")[0] , "--data", req.get_data().decode()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.run(["curl", "-X", request.method, url+path,"-H","Content-Type:" + request.headers.getlist("Content-Type")[0] , "--data", request.get_data().decode()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     except:
-        proc = subprocess.run(["curl", "-X", req.method, url+path, "--data", req.get_data().decode()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.run(["curl", "-X", request.method, url+path, "--data", request.get_data().decode()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     return Response(proc.stdout)
 
