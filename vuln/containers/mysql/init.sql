aeafcd108f13978f453cc4e3bf0cc6f225ce292f
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-11-27 07:12:30 +0900

    fix fucking typo that is typed by monkey

--- vuln/containers/mysql/init.sql
+++ vuln/containers/mysql/init.sql
@@ -7,5 +7,5 @@ create table docker (
     pass char(50)
 );
 insert into docker values ("admin", "P@ssw0rd"); 
-insert into docker values ("yoden", "Svp3rS3cr3P4sswr0d!!!"); 
+insert into docker values ("yoden", "Svp3rS3crt3P4sswr0d!!!"); 
 insert into docker values ("flag", "flag{7h1s_1s_f4k3_fl4g}"); 
